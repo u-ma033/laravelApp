3750a9d9af95e6084655af2f4030dcdec72f4d9a
uchizumimasato <64341031+u-mma@users.noreply.github.com>
2021-05-22 14:00:26 +0900

    投稿画面作成

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('posts/create', function() {
+
+    return view('posts.create');
+});
+
+Route::resource('posts', 'PostsController', ['only' => ['store']]);
